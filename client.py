be89db30106966d4ad5f9b1471f2e207eb92e30c
Maximux <max@maximux.com>
2017-01-26 22:52:06 +0000

    Improved client

--- client.py
+++ client.py
@@ -1,10 +1,24 @@
-import request
+import requests
 import json
 import argparse
 
 URL = 'http://localhost:5000/api/v1/'
 HOST = 'localhost'
 PORT = 5000
+
+class Market(object):
+    
+    def _post(self,path,data):
+        path = URL+ "books/" + path
+        return requests.post(path,data=json.dumps(data))
+    
+    def _get(self,path,glob=False):
+        path = URL+ "books/" + path
+        return requests.get(path)
+    
+    def get_book(self,asset):
+        response = self._get(asset)
+        return response.json()
 
 class Client(object):
     def __init__(self,name):
@@ -24,12 +38,41 @@ class Client(object):
         return response.json()
         
     def get_balance(self):
-        response = self._get('balance'.format(self.id))
+        response = self._get('balance')
+        return response.json()
+    
+    def get_assets(self):
+        response = self._get('assets')
         return response.json()
     
     def add_asset(self,asset,quantity):
         response = self._post("add_asset",{"asset":asset,"quantity":quantity})
-        return response
+        return response.json()
+    
+    def credit(self,amount):
+        response = self._post("credit",{"amount":amount})
+        return response.json()
+    
+    def post_order(self,asset,side,price,volume):
+        order = {
+            'asset':asset,
+            'side':side,
+            'price':price,
+            'volume':volume
+        }
+        response = self._post("post_order",order)
+        return response.json()
+    
+    def take_order(self,asset,side,price,volume):
+        order = {
+            'asset':asset,
+            'side':side,
+            'price':price,
+            'volume':volume
+        }
+        response = self._post("take_order",order)
+        return response.json()
+        
 
 def main():
 	pass
